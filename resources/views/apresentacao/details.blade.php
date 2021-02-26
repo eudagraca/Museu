2f83621d7a1356eb7c52f5f35624edf102e8ab49
euclidiodagraca <euclidiodagraca@gmail.com>
2021-02-26 12:28:10 +0200

    “Coreccoes”

--- resources/views/apresentacao/details.blade.php
+++ resources/views/apresentacao/details.blade.php
@@ -4,48 +4,59 @@
 
 <div class="uk-container uk-margin-large-bottom uk-margin-large-top">
 
-<div class="uk-flex ukflex-center uk-align-right">
+    <div class="uk-flex ukflex-center uk-align-right">
 
- <a class="uk-button uk-button-primary" href="#apresentacao">Apresentação da EDM</a>
- <a class="uk-button uk-button-primary" href="#governanca">Modelo de governança</a>
- <a class="uk-button uk-button-primary" href="#organizacao">Estrutura Organizacional</a>
-</div>
+        <a class="uk-button uk-button-primary" href="#apresentacao">Apresentação da EDM</a>
+        <a class="uk-button uk-button-primary" href="#governanca">Modelo de governança</a>
+        <a class="uk-button uk-button-primary" href="#organizacao">Estrutura Organizacional</a>
+    </div>
     <div class="uk-width-1-1@m" id="apresentacao">
         <h3 class="uk-heading-bullet uk-text-bolder uk-text-primary">Apresentação da EDM</h3>
 
         <p class="uk-dropcap uk-text uk-margin-left uk-margin-right uk-text-justify">
-           A Empresa Nacional de Electricidade de Moçambique Empresa Estatal, (EDM-E.E) foi criada em 27 de Agosto de 1977, através
-        do Decreto Lei nº 38/77 de 27 de Agosto, que resultou da integração de vários Serviços Municipais de Água e
-        Electricidade (SMAE), Sociedade Hidro-Eléctrica do Revué (SHER), Sociedade Nacional de Estudos e Financiamentos de
-        Empreendimentos Ultramarinos (SONEFE), Serviços Municipalizados de Electricidade (SME) e Serviços de Água e
-        Electricidade (SAE). Em 1995 a EDM foi transformada numa empresa pública, ao abrigo do decreto nº 28/95, com a
-        designação da EDM-Electricidade de Moçambique, Empresa Pública (EDM – E.P).
+            A Empresa Nacional de Electricidade de Moçambique Empresa Estatal, (EDM-E.E) foi criada em 27 de Agosto de
+            1977, através
+            do Decreto Lei nº 38/77 de 27 de Agosto, que resultou da integração de vários Serviços Municipais de Água e
+            Electricidade (SMAE), Sociedade Hidro-Eléctrica do Revué (SHER), Sociedade Nacional de Estudos e
+            Financiamentos de
+            Empreendimentos Ultramarinos (SONEFE), Serviços Municipalizados de Electricidade (SME) e Serviços de Água e
+            Electricidade (SAE). Em 1995 a EDM foi transformada numa empresa pública, ao abrigo do decreto nº 28/95, com
+            a
+            designação da EDM-Electricidade de Moçambique, Empresa Pública (EDM – E.P).
         </p>
 
-        <p class="uk-text uk-margin-left uk-margin-right uk-text-justify">O sector de energia é considerado prioritário a nível do Governo, que está empenhado em levar a cabo medidas para atrair
-        o investimento privado nesta área e promover o desenvolvimento da rede nacional de energia.</p>
+        <p class="uk-text uk-margin-left uk-margin-right uk-text-justify">O sector de energia é considerado prioritário
+            a nível do Governo, que está empenhado em levar a cabo medidas para atrair
+            o investimento privado nesta área e promover o desenvolvimento da rede nacional de energia.</p>
 
-        <p class="uk-text uk-margin-left uk-margin-right uk-text-justify">Em Outubro de 1997, acompanhando as tendências internacionais que apontavam para uma maior liberalização dos mercados, é
-        promulgado a Lei de Electricidade (Lei 21/97 de 1 de Outubro) que altera alguns princípios presentes no quadro nacional
-        e aprova a regulamentação específica que estabelece a Política Energética (resolução 05/98 de 3 de Março), que confere à
-        EDM o papel de Gestor da Rede Nacional de Transporte de Energia Eléctrica e o Regulamento da Lei de Electricidade que
-        define as competências e os procedimentos relativos à atribuição de concessões de Produção, Transporte, Distribuição e
-        Comercialização de Energia Eléctrica.</p>
+        <p class="uk-text uk-margin-left uk-margin-right uk-text-justify">Em Outubro de 1997, acompanhando as tendências
+            internacionais que apontavam para uma maior liberalização dos mercados, é
+            promulgado a Lei de Electricidade (Lei 21/97 de 1 de Outubro) que altera alguns princípios presentes no
+            quadro nacional
+            e aprova a regulamentação específica que estabelece a Política Energética (resolução 05/98 de 3 de Março),
+            que confere à
+            EDM o papel de Gestor da Rede Nacional de Transporte de Energia Eléctrica e o Regulamento da Lei de
+            Electricidade que
+            define as competências e os procedimentos relativos à atribuição de concessões de Produção, Transporte,
+            Distribuição e
+            Comercialização de Energia Eléctrica.</p>
 
         <div class="uk-margin-left uk-margin-right">
             <h4>Objecto do Negócio da EDM</h4>
             <ul class="uk-list uk-list-disc uk-list-primary uk-margin-right uk-margin-left">
-                <li class="uk-text-black">Estabelecimento e exploração dos meios de produção próprios e dos que fazem parte do património do Estado colocado à
-                disposição da EDM, afectos à produção de energia eléctrica</li>
-                <li class="uk-text-black">Transformação, conversão, transporte, distribuição e comercialização de energia eléctrica no território nacional e
-                fora dele;</li>
-                <li class="uk-text-black">Gestão e operação da rede nacional de transporte de energia eléctrica, na qualidade de Gestor de Rede Nacional de
-                Transporte (RNT) e</li>
-                <li class="uk-text-black">Realização de trabalhos de instalação, de reparação e de renovação de bens afectos a exploração do serviço público.</li>
+                <li class="uk-text-black">Estabelecimento e exploração dos meios de produção próprios e dos que fazem
+                    parte do património do Estado colocado à
+                    disposição da EDM, afectos à produção de energia eléctrica</li>
+                <li class="uk-text-black">Transformação, conversão, transporte, distribuição e comercialização de
+                    energia eléctrica no território nacional e
+                    fora dele;</li>
+                <li class="uk-text-black">Gestão e operação da rede nacional de transporte de energia eléctrica, na
+                    qualidade de Gestor de Rede Nacional de
+                    Transporte (RNT) e</li>
+                <li class="uk-text-black">Realização de trabalhos de instalação, de reparação e de renovação de bens
+                    afectos a exploração do serviço público.</li>
             </ul>
         </div>
-
-
     </div>
 
     <hr>
@@ -54,7 +65,8 @@
         <h4 class="uk-heading-bullet uk-text-bolder uk-text-primary">Missão da EDM</h4>
 
         <p class="uk-text uk-margin-left uk-margin-right uk-text-justify">
-            Produzir, transportar, distribuir e comercializar energia eléctrica de boa qualidade, de forma sustentável, para
+            Produzir, transportar, distribuir e comercializar energia eléctrica de boa qualidade, de forma sustentável,
+            para
             iluminar e potenciar a industrialização do país.
         </p>
     </div>
@@ -63,12 +75,13 @@
         <h4 class="uk-heading-bullet uk-text-bolder uk-text-primary">Visão</h4>
 
         <p class="uk-text uk-margin-left uk-margin-right uk-text-justify">
-            Transformar a EDM numa Utilidade Inteligente e Sustentável, que dá acesso à energia eléctrica de qualidade a cada
+            Transformar a EDM numa Utilidade Inteligente e Sustentável, que dá acesso à energia eléctrica de qualidade a
+            cada
             moçambicano e exerce liderança no Mercado Regional.
         </p>
     </div>
 
-    <div class="uk-width-1-1@m" >
+    <div class="uk-width-1-1@m">
         <h4 class="uk-heading-bullet uk-text-bolder uk-text-primary">Valores</h4>
 
         <p class="uk-text uk-margin-left uk-margin-right uk-text-justify">
@@ -92,6 +105,7 @@
             Produção, Transporte, Distribuição e Comercialização de Energia Eléctrica e para a
             prossecução da sua actividade, estabeleceu um modelo de gestão que compreende três níveis apresentados na
             figura abaixo:</p>
+
 
         <div class="uk-grid">
             <div class="uk-width-1-3@m uk-align-center">
@@ -163,11 +177,11 @@
         <h3 class="uk-heading-bullet uk-text-bolder uk-text-primary">Estrutura Organizacional de Alto Nível</h3>
 
         <div class="uk-width-1-2@m uk-align-center" uk-lightbox="animation: slide">
-            <a class="uk-inline" href="{{ secure_asset('assets/images/estrutura_organizacional.png') }}" data-caption="Estrutura Organizacional de Alto Nível">
-            <img src="{{ secure_asset('assets/images/estrutura_organizacional.png') }}" class="uk-border-rounded"
-                width="850px" height="533px" alt="" uk-img>
+            <a class="uk-inline" href="{{ secure_asset('assets/images/estrutura_organizacional.png') }}"
+                data-caption="Estrutura Organizacional de Alto Nível">
+                <img src="{{ secure_asset('assets/images/estrutura_organizacional.png') }}" class="uk-border-rounded"
+                    width="850px" height="533px" alt="" uk-img></a>
         </div>
     </div>
-
 </div>
 @endsection
