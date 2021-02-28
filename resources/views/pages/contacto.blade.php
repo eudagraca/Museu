d9509b0e47f4f9392b8d876d9cbc49b5fdc19acc
euclidiodagraca <euclidiodagraca@gmail.com>
2021-02-28 17:42:24 +0200

    New version

--- resources/views/pages/contacto.blade.php
+++ resources/views/pages/contacto.blade.php
@@ -52,13 +52,16 @@
                 <div class="uk-margin-small">
                     <label class="uk-form-label" for="form-stacked-text">Localização | endereço</label>
                     <div class="uk-form-controls">
-                        <label id="form-stacked-text" >Av, X, Cidade de Maputo, Moçambique</label>
+
+                        <label id="form-stacked-text" >Direção da Academia da EDM</label>
+                        <label id="form-stacked-text">Av, Eduardo Mondlane nº 1352, 2º Andar, Maputo - Moçambique</label>
                     </div>
                 </div>
                 <div class="uk-margin-small">
                     <label class="uk-form-label" for="form-stacked-text">Telefone</label>
                     <div class="uk-form-controls">
-                        <label id="form-stacked-text" >+2588400000000</label>
+                        <label id="form-stacked-text" >+258 843400098</label>
+                        <label id="form-stacked-text" >+258 829970091</label>
                     </div>
                 </div>
 
